5efcd381d77c744672488d6a33126cbc4489bbda
Hudson Israel <hudson.israel716@gmail.com>
2022-08-02 20:35:10 -0300

    Introduction to for, break, else if.
    Added settings, more functions and logic.

--- Jogo de advinhação.cpp
+++ Jogo de advinhação.cpp
@@ -8,22 +8,37 @@ int main() {
 
     int numrandom = 42;
     int guess;
+    int attempts;
 
-    printf("what's your kick?: ");
-    scanf("%d", &guess);
-    printf("your kick was %d\n", guess);
+    printf("Enter the number of tries you can hit the secret number: ");
+    scanf("%d", &attempts);
+    
+    for (int i = 1; i <= attempts; i++) {
+        
+        int possibility = attempts - 1;
+        int hit = (guess == numrandom);
 
-    if (guess == numrandom) {
-        printf("congratulations!!, your guess is correct!");
-    }
-    else {
-        if(guess > numrandom) {
-            printf("His guess was bigger than the secret number.");
+        printf("what's your kick?: ");
+        scanf("%d", &guess);
+        printf("your kick was %d\n", guess);
+        
+        if (guess < 0){
+            printf("Negative numbers cannot, you still have %d tries.\n", possibility);
+            i--;
+            continue;
+        }
+        if (hit) {
+            printf("congratulations!!, your guess is correct!\n");
+            break;
         }
         else {
-            printf("His guess was less than the secret number.");
+            if(guess > numrandom) {
+                printf("His guess was bigger than the secret number.\n");
+            }
+            else {
+                printf("His guess was less than the secret number.\n");
+            }
+            printf("You failed, but you can still go on.\n");
         }
-        printf("You made a mistake, try again! :D");
     }
 }
-
