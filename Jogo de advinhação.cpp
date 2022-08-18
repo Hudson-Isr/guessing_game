48ccb3974cc94e112f17364d37d09a1535880b10
Hudson Israel <hudson.israel716@gmail.com>
2022-08-18 15:53:43 -0300

    Update
    Included scoring system per attempt, random numbers.

--- Jogo de advinhação.cpp
+++ Jogo de advinhação.cpp
@@ -1,46 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
     
     printf("********************************\n");
     printf("* Welcome to our guessing game *\n");
     printf("********************************\n");
+    printf("\n");
 
-    int numrandom = 42;
+    srand(time(NULL));
+    int numrandom = rand()%100;
     int guess;
     int attempts = 0;
     int has_won = 0;
-    int score = 1000;
+    double score = 1000;
 
     while(has_won == 0){
 
         printf("attempts %d\n", attempts+1);
-        printf("what's your kick?: \n");
+        printf("what's your kick?: ");
         scanf("%d", &guess);
+        printf("\n");
         printf("your kick was %d\n", guess);
         
         int hit = (guess == numrandom);
 
         if (guess < 0){
             printf("Negative numbers cannot.\n");
+            printf("\n");
             continue;
         }
         if (hit) {
             printf("congratulations!!, your guess is correct!\n");
+            printf("\n");
             has_won = 1;
         }
         else {
             if(guess > numrandom) {
                 printf("His guess was bigger than the secret number.\n");
+                printf("\n");
             }
             else {
                 printf("His guess was less than the secret number.\n");
+                printf("\n");
             }
         }
         attempts++;
-        int score_down = (guess - numrandom) / 2;
+        double score_down = abs(guess - numrandom) / 2.0;
         score = score - score_down;
     }
     printf("You got it right in %d attempts.\n", attempts);
-    printf("Total Score: %d\n", score);
+    printf("Total Score: %.2f\n", score);
 }
